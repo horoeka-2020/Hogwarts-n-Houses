f911a49390fa2e3a893c5a7e093ca48ea6e99172
Tai-hona <70182745+tai-hona@users.noreply.github.com>
2020-10-23 11:03:21 +1300

    page is working

--- Routes.js
+++ Routes.js
@@ -1,8 +1,22 @@
+const fs = require('fs')
+const path = require('path')
+
 const express = require('express')
 const router = express.Router()
 
-
-
 router.use(express.urlencoded({extended: false}))
 
+const filePath = path.join(__dirname, 'names.json')
+
 module.exports = router
+
+router.get('/', (req, res) => {
+    fs.readFile(filePath, 'utf8', (err, contents) => {
+        if (err) return res.sendStatus(500)
+        const names = JSON.parse(contents)
+        const viewHouses = {
+            houses: names.houses
+        }
+    })
+    res.render('home')
+  })
--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express')
 const hbs = require('express-handlebars')
 
 const server = express()
+const routes = require('./Routes')
 
 server.use(express.static('public'))
 server.use(express.urlencoded({extended: false}))
 
-server.engine('hbs', hbs({extname: 'hbs'}))
-server.set('view engin', 'hbs')
 
+server.engine('hbs', hbs({extname: 'hbs'}))
+server.set('view engine', 'hbs')
+
+
+server.use('/', routes)
 module.exports = server
