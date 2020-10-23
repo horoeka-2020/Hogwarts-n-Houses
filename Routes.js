2166a7ea87427d85167f33fb135acdecf5702450
Tai-hona <70182745+tai-hona@users.noreply.github.com>
2020-10-23 13:56:46 +1300

    more things

--- Routes.js
+++ Routes.js
@@ -11,25 +11,62 @@ const filePath = path.join(__dirname, 'names.json')
 module.exports = router
 
 router.get('/', (req, res) => {
-    fs.readFile(filePath, 'utf8', (err, contents) => {
-        if (err) return res.sendStatus(500)
-        const names = JSON.parse(contents)
-        const viewHouses = {
-            houses: names.houses
-        }
-    })
+    // fs.readFile(filePath, 'utf8', (err, contents) => {
+    //     if (err) return res.sendStatus(500)
+    //     const names = JSON.parse(contents)
+    //     const viewHouses = {
+    //         houses: names.houses
+    //     }
+    // })
     res.render('home')
   })
 
-  router.get('/houses/:id', (req, res) => {
-      const id = Number(req.params.id)
+  router.get('/:id', (req, res) => {
+      const houseId = Number(req.params.id)
+    //   console.log(houseId)
       fs.readFile(filePath, 'utf8', (err, contents) => {
+          
         if (err) return res.sendStatus(500)
+
         const names = JSON.parse(contents)
-        const houseNames = names.Houses.find(val => val.id === id)
+        const houseNames = names.Houses.find(({id})=>id === houseId)
+        // console.log(houseNames)
         const viewHouse = {
-            name: houseNames.id
+
+            id: houseNames.id,
+            image: houseNames.image
         }
         res.render('houses', viewHouse)
       })
+      
   })
+
+  
+  router.post('/houses/:id', (req, res) => {
+      const addName = req.body
+      const id = Number(req.params.id)
+      fs.readFile(filePath, 'utf8', (err, contents) => {
+          if (err) return res.sendStatus(500)
+          const moreNames = JSON.parse(contents)
+          
+          console.log(moreNames.Houses[id-1].name)
+          
+          const newContents = JSON.stringify(moreNames, null, 2)
+          fs.writeFile(filePath, newContents, 'utf8', (err) => {
+              if (err) return res.sendStatus(500)
+              res.redirect('/' + id)
+            })
+        })
+    })
+    // router.get('/display', (req, res) => {
+    //     const id = Number(req.params.id)
+    //     fs.readFile(filePath, 'utf8', (err, contents) => {
+    //       if (err) return res.sendStatus(500)
+    //       const names = JSON.parse(contents)
+    //       const houseNames = names.Houses.find(val => val.id === id)
+    //       const viewHouse = {
+    //           name: houseNames.id
+              
+    //       })
+
+    // })
